f82bf3d5f2674fc8ec08afb2695479470160b27d
Abdoulaye DIA <abdoudu78130@gmail.com>
2022-10-14 01:18:05 +0200

    init project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "xml_parser.hpp"
+
+int main(int argc, char **argv)
+{
+
+    std::cout << "Hello World !" << std::endl;
+
+    return 0;
+}
---
+++ xml_parser.hpp
@@ -0,0 +1,17 @@
+#ifndef XML_PARSER
+#define XML_PARSER
+
+#pragma once
+
+#include <iostream>
+
+class XMLParser
+{
+private:
+    /* data */
+public:
+    XMLParser(/* args */) {}
+    ~XMLParser() {}
+};
+
+#endif // !XML_PARSER
